368586f2eecf129bb7be2d9b97886f75c9ed7357
IonicBowl <133007178+IonicBowl@users.noreply.github.com>
2023-05-09 13:20:22 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 info.setScore(0)
 tiles.setCurrentTilemap(tilemap`level`)
+music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
 forever(function () {
     if ((0 as any) == (true as any)) {
         info.changeScoreBy(1)

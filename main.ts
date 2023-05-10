3734cc37ffcf1c8a5684b55115ce7353a46b1ca5
IonicBowl <133007178+IonicBowl@users.noreply.github.com>
2023-05-10 08:53:54 +0200

    Test

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
+scene.setBackgroundColor(0)
 info.setScore(0)
 tiles.setCurrentTilemap(tilemap`level`)
 music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
 forever(function () {
     if ((0 as any) == (true as any)) {
         info.changeScoreBy(1)
+    }
+})
+forever(function () {
+    if (controller.A.isPressed()) {
+        music.play(music.createSong(assets.song`USP-S1`), music.PlaybackMode.InBackground)
+        pause(400)
     }
 })

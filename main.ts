c575f642707006ef34d90bf06821bc50cab41eeb
IonicBowl <133007178+IonicBowl@users.noreply.github.com>
2023-05-23 13:13:32 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 scene.setBackgroundColor(0)
-let Ak47 = 1
+let M4A1S = 1
 info.setScore(0)
 forever(function () {
     let USPS = 0
@@ -11,10 +11,19 @@ forever(function () {
     }
 })
 forever(function () {
+    let Ak47 = 0
     if (Ak47 == 1) {
         if (controller.A.isPressed()) {
             music.play(music.createSong(assets.song`AK-47`), music.PlaybackMode.InBackground)
             pause(75)
+        }
+    }
+})
+forever(function () {
+    if (M4A1S == 1) {
+        if (controller.A.isPressed()) {
+            music.play(music.createSong(assets.song`M4A1-S`), music.PlaybackMode.InBackground)
+            pause(80)
         }
     }
 })
